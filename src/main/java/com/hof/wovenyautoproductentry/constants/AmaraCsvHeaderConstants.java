a4feafff1c7784151c99ff453e2d8c5f75d19cc8
Fatih YILMAZ <fatih22yilmaz@gmail.com>
2020-03-15 18:40:52 +0300

    adds amara initialization (#15)
    * adds amara db initialization --incomplete
    
    * adds amara products db initialization

---
+++ src/main/java/com/hof/wovenyautoproductentry/constants/AmaraCsvHeaderConstants.java
@@ -0,0 +1,41 @@
+package com.hof.wovenyautoproductentry.constants;
+
+public class AmaraCsvHeaderConstants {
+
+    public static final String SKU_NUMBER = "sku_number";
+    public static final String NAME = "name";
+    public static final String META_DESCRIPTION = "meta_description";
+    public static final String PRODUCT_STYLES1 = "product_styles1";
+    public static final String PRODUCT_STYLES2 = "product_styles2";
+    public static final String PRODUCT_STYLES3 = "product_styles3";
+    public static final String WEAVE = "weave";
+    public static final String PRODUCT_METARIALS1 = "product_metarials1";
+    public static final String PRODUCT_METARIALS2 = "product_metarials2";
+    public static final String PRODUCT_COLORS1 = "product_colors1";
+    public static final String PRODUCT_COLORS2 = "product_colors2";
+    public static final String PRODUCT_COLORS3 = "product_colors3";
+    public static final String PRODUCT_COLORS4 = "product_colors4";
+    public static final String PATTERN = "pattern";
+    public static final String REGION = "region";
+    public static final String WIDTH_BY_FEET_F = "width_by_feet-f";
+    public static final String WIDTH_BY_FEET_I = "width_by_feet-i";
+    public static final String WIDTH_BY_INCHES = "width_by_inches";
+    public static final String LENGTH_BY_FEET_F = "length_by_feet-f";
+    public static final String LENGTH_BY_FEET_I = "length_by_feet-i";
+    public static final String LENGTH_BY_INCHES = "length_by_inches";
+    public static final String HEIGHT = "height";
+    public static final String AGE = "age";
+    public static final String PERIOD_MADE = "period_made";
+    public static final String IS_USED = "is_used";
+    public static final String HAS_IMPERFECTION = "has_imperfection";
+    public static final String CONDITION_DETAILS = "condition_details";
+    public static final String PRICE = "price";
+    public static final String DISCOUNT = "discount";
+    public static final String RETAIL_PRICE = "retail_price";
+    public static final String IS_LOCAL_PICKUP = "is_local_pickup";
+    public static final String LOCAL_DELIVERY_MILES = "local_delivery_miles";
+    public static final String PICKUP_CHARGE = "pickup_charge";
+    public static final String IS_OWN_SHIPPING = "is_own_shipping";
+    public static final String IS_FREE_SHIPPING = "is_free_shipping";
+    public static final String STATUS = "status";
+}
